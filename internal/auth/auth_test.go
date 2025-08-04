28f382317704c4e9192ad1348e474ad98d93eee4
Evenus11 <ewen.shackel@gmail.com>
2025-08-04 18:25:27 +0100

    adding formating tests

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,36 +1,36 @@
 package auth
 
 import (
-	  "errors"
-	  "github.com/google/go-cmp/cmp"
-	  "net/http"
-	  "testing"
+	"errors"
+	"github.com/google/go-cmp/cmp"
+	"net/http"
+	"testing"
 )
 
 func TestAuth(t *testing.T) {
-	  tests := map[string]struct {
-			input   http.Header
-			want    string
-			wantErr error
-	  }{
-			"basic":           {input: http.Header{"Authorization": []string{"ApiKey Test-Key "}}, want: "Test-Key", wantErr: nil},
-			"no auth hedder":  {input: http.Header{" ": []string{"ApiKey Test-Key"}}, want: "", wantErr: ErrNoAuthHeaderIncluded},
-			"no key included": {input: http.Header{"Authorization": []string{"ApiKey"}}, want: "", wantErr: errors.New("malformed authorization_ eader")},
-	  }
+	tests := map[string]struct {
+		input   http.Header
+		want    string
+		wantErr error
+	}{
+		"basic":           {input: http.Header{"Authorization": []string{"ApiKey Test-Key "}}, want: "Test-Key", wantErr: nil},
+		"no auth hedder":  {input: http.Header{" ": []string{"ApiKey Test-Key"}}, want: "", wantErr: ErrNoAuthHeaderIncluded},
+		"no key included": {input: http.Header{"Authorization": []string{"ApiKey"}}, want: "", wantErr: errors.New("malformed authorization_ eader")},
+	}
 
-	  for name, tc := range tests {
-			t.Run(name, func(t *testing.T) {
-				  got, err := GetAPIKey(tc.input)
-				  if tc.wantErr != nil && err == nil {
-						t.Fatalf("GetAPIKey should have returned an error, but didn't")
-				  } else if tc.wantErr == nil && err != nil {
-						t.Fatalf("GetAPIKey should not have returned an error, but did: %v", err)
-				  }
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, err := GetAPIKey(tc.input)
+			if tc.wantErr != nil && err == nil {
+				t.Fatalf("GetAPIKey should have returned an error, but didn't")
+			} else if tc.wantErr == nil && err != nil {
+				t.Fatalf("GetAPIKey should not have returned an error, but did: %v", err)
+			}
 
-				  diff := cmp.Diff(tc.want, got)
-				  if diff != "" {
-						t.Fatalf(diff)
-				  }
-			})
-	  }
+			diff := cmp.Diff(tc.want, got)
+			if diff != "" {
+				t.Fatalf(diff)
+			}
+		})
+	}
 }
